f77f5246b39e5969fd5eed00c21364f19d183316
Ian Luites <IanLuites@users.noreply.github.com>
2019-10-18 20:40:39 +0200

    Redo encoding.

--- lib/kvasir/command/encoder.ex
+++ lib/kvasir/command/encoder.ex
@@ -1,22 +1,56 @@
 defmodule Kvasir.Command.Encoder do
-  def encode(command, opts \\ []), do: encoding(opts).encode(command, opts)
-  def decode(command, opts \\ []), do: encoding(opts).decode(command, opts)
+  alias Kvasir.Type.Serializer
 
-  @base_encoders %{
-    brod: Kvasir.Command.Encodings.Brod,
-    json: Kvasir.Command.Encodings.JSON,
-    raw: Kvasir.Command.Encodings.Raw
-  }
+  def encode(command, _opts \\ []) do
+    with {:ok, payload} <- payload(command) do
+      {:ok,
+       %{
+         type: type(command),
+         meta: meta(command),
+         payload: payload
+       }}
+    end
+  end
 
-  @spec encoding(Keyword.t()) :: module
-  defp encoding(opts) do
-    encoding = opts[:encoding]
+  def decode(data, opts \\ []) do
+    with {:ok, command} <- find_command(MapX.get(data, :type)),
+         {:ok, payload} <-
+           Serializer.decode(command.__command__(:fields), MapX.get(data, :payload)) do
+      cmd =
+        Map.put(
+          payload,
+          :__meta__,
+          Kvasir.Command.Meta.decode(MapX.get(data, :meta), opts[:meta])
+        )
 
-    cond do
-      is_nil(encoding) -> Kvasir.Command.Encodings.JSON
-      :erlang.function_exported(encoding, :encode, 2) -> encoding
-      encoding = @base_encoders[encoding] -> encoding
-      :no_valid_given -> {:error, :invalid_encoding}
+      case opts[:process] do
+        :create -> command.create(cmd)
+        :struct -> struct!(command, cmd)
+        nil -> struct!(command, cmd)
+      end
+    end
+  end
+
+  ### Helpers ###
+  ## Encoding  ##
+
+  defp type(%command{}), do: command.__command__(:type)
+  defp meta(%{__meta__: meta}), do: Kvasir.Command.Meta.encode(meta)
+  defp payload(command = %type{}), do: Serializer.encode(type.__command__(:fields), command)
+
+  ## Decoding  ##
+
+  defp find_command(command) when is_atom(command) do
+    if Code.ensure_compiled?(command),
+      do: {:ok, command},
+      else: {:error, :unknown_command}
+  end
+
+  defp find_command(command) when is_binary(command) do
+    if lookup = Kvasir.Command.Registry.lookup(command) do
+      find_command(lookup)
+    else
+      find_command(Module.concat("Elixir", command))
     end
   end
 end
