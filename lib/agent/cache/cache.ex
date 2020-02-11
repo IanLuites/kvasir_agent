ed29416b2c2f0178bf43c0d0a4492832c0688547
Ian Luites <IanLuites@users.noreply.github.com>
2020-02-11 23:40:43 +0100

    Add cache delete.

--- lib/agent/cache/cache.ex
+++ lib/agent/cache/cache.ex
@@ -3,4 +3,6 @@ defmodule Kvasir.Agent.Cache do
   @callback load(agent :: module, id :: term) :: {:ok, Kvasir.Offset.t(), map} | {:error, atom}
   @callback save(agent :: module, id :: term, data :: map, offset :: Kvasir.Offset.t()) ::
               :ok | {:error, atom}
+  @callback delete(agent :: module, id :: term) ::
+              :ok | {:error, atom}
 end
