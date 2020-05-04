af292160d23997217eb885f6a6a92257ec2259a9
Ian Luites <IanLuites@users.noreply.github.com>
2020-05-04 13:07:38 +0200

    Fix restart supervisor ID.

--- lib/agent/registry/local.ex
+++ lib/agent/registry/local.ex
@@ -3,16 +3,17 @@ defmodule Kvasir.Agent.Registry.Local do
 
   @impl Kvasir.Agent.Registry
   def start_child(agent, partition, id) do
+    child = {:agent, id}
     via_name = {:via, Registry, {agent.__registry__(partition), id}}
     supervisor = agent.__supervisor__(partition)
 
     case Supervisor.start_child(supervisor, %{
-           id: {:agent, id},
+           id: child,
            start: {Kvasir.Agent.Instance, :start_agent, [agent, partition, id, [name: via_name]]},
            restart: :transient
          }) do
       {:error, {:already_started, pid}} -> {:ok, pid}
-      {:error, :already_present} -> Supervisor.restart_child(supervisor, id)
+      {:error, :already_present} -> Supervisor.restart_child(supervisor, child)
       reply -> reply
     end
   end
